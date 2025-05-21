f7506fd18f252f2d63fe387c1bdbffa86f07d63e
zirkonya <zirkonya@zircon>
2025-05-21 21:15:22 +0200

    remove comment

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,6 @@ use error::AtmError;
 mod atm;
 mod error;
 
-/*
-        l'ATM tu as un stock de départ (tant de billets de 5, tant de 10, etc).
-        Tu dois pouvoir prendre des inputs successifs où l'utilisateur demande à retirer un montant,
-        tu gères si c'est possible (genre 22 c'est pas possible si plus petite coupure = 5,
-        mais aussi peut-être qu'au bout d'un moment il y a rupture d'un certain type de billets),
-        et tu gères ce que ça retire du stock.
-
-    ATM :
-        - : nombres de coupures
-        - : vérifier si c'est possible
-            - multiple de la plus petite coupure disponible
-        - : vérifier le stock
-            - <= au maximum disponible
-            - ou possible d'atteindre la valeur avec les coupures disponible
-        - : mettre à jour le stock
-*/
-
 const FIVE: Denomination = Denomination::new(5, 10);
 const TEN: Denomination = Denomination::new(10, 10);
 const TWENTY: Denomination = Denomination::new(20, 10);
