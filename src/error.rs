c9f4227137f7e1f87ee2d83a2d756ec77a2f9bf0
zirkonya <zirkonya@zircon>
2025-05-27 21:48:18 +0200

    correcting spelling mistakes

--- src/error.rs
+++ src/error.rs
@@ -16,7 +16,7 @@ pub enum AtmError {
 impl Display for AtmError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            AtmError::InsuficientBalance { amount, balance } => write!(f, "Insuficient balance you tried {amount}€ but {balance}€ avaiable"),
+            AtmError::InsuficientBalance { amount, balance } => write!(f, "Insuficient balance you tried {amount}€ but {balance}€ available"),
             AtmError::DenominationNotAvailable { amount, denomination } => write!(f, "No denomination available to give you {amount}€ the smallest denomination available is {denomination}€."),
             AtmError::AtmIsEmpty => write!(f, "Atm is empty"),
         }
